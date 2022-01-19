15182e174c49f95f0416bfda0e17f39b4c3ca77b
dovilivob <dovilivob@dovilivob-System-Product-Name>
2022-01-19 20:26:07 +0800

    !

--- Expression/src/Main.java
+++ Expression/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.print("Hello");
 		System.out.print("cool");
-		System.out.print("very cool");
+		System.out.print("very cool!");
 	}
 
 }
